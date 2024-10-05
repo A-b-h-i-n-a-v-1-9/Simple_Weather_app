1eca8adfbaec86b751b89bb098331dfc285913e9
Abhinav Mehta <144078110+A-b-h-i-n-a-v-1-9@users.noreply.github.com>
2024-10-05 18:44:48 +0530

    blank app.js

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-function getWeather() {
-    const location = document.getElementById('location').value;
-    if (location) {
-        document.querySelector('.city').innerText = location;
-        // You can integrate an API here to dynamically fetch temperature, humidity, etc.
-        alert(`Fetching weather for ${location}...`);
-    } else {
-        alert('Please enter a city.');
-    }
-}
+
